5cc56e58a840d7f4b6bf3a22f4105c53c3a3351b
Devon Vendetti <v3nd3tti@icloud.com>
2025-09-09 19:20:05 -0700

    Add some JSInterop functionality with Sweet Alert.

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -21,8 +21,9 @@ public class Pizza
 
     public List<PizzaTopping> Toppings { get; set; } = default!;
 
-    public decimal GetBasePrice() =>
-        (decimal)Size / DefaultSize * Special?.BasePrice ?? 1;
+    public decimal GetBasePrice() => Special is { FixedSize: not null }
+        ? Special.BasePrice
+        : (decimal)Size / DefaultSize * Special?.BasePrice ?? 1;
 
     public decimal GetTotalPrice() => GetBasePrice();
 
